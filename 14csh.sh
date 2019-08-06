94d5febff42a9e639681bbf8ce24b1c89ba445d5
amsherwin <53815042+amsherwin@users.noreply.github.com>
2019-08-06 17:27:01 +0800

    14场脚本
    14场测试

---
+++ 14csh.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+for aa in {3,1,0}
+do
+        for bb in {3,1,0}
+        do
+                for cc in {3,1,0}
+                do
+                        for dd in {3,1,0}
+                        do
+                                for ee in {3,1,0}
+                                do
+                                        for ff in {3,1,0}
+                                        do
+                                                for gg in {3,1,0}
+                                                do
+                                                        for hh in {3,1,0}
+                                                        do
+                                                                for ii in {3,1,0}
+                                                                do
+                                                                        for jj in {3,1,0}
+                                                                        do
+                                                                                for kk in {3,1,0}
+                                                                                do
+                                                                                        for ll in {3,1,0}
+                                                                                        do
+                                                                                                for mm in {3,1,0}
+                                                                                                do
+                                                                                                        for nn in {3,1,0}
+                                                                                                        do
+                                                                                                        echo  $aa  $bb  $cc  $dd  $ee  $ff  $gg  $hh  $ii  $jj  $kk  $ll  $mm  $nn a$(($aa +$bb +$cc +$dd +$ee +$ff +$gg +$hh +$ii +$jj +$kk +$ll +$mm+$nn))a
+                                                                                                        done
+                                                                                                done
+                                                                                        done
+                                                                                done
+                                                                        done
+                                                                done
+                                                        done
+                                                done
+                                        done
+                                done
+                        done
+                   done
+        done
+done
